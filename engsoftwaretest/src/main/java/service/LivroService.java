5ab4e5b8d01a2974ad17184bd4832b906e50049a
JPBandeira <joaopedro980804@gmail.com>
2019-06-01 21:08:40 -0300

    Fazendo novas alterações nas classes de livro livro

--- engsoftwaretest/src/main/java/service/LivroService.java
+++ engsoftwaretest/src/main/java/service/LivroService.java
@@ -10,49 +10,30 @@ public class LivroService {
 	private List<LivroModel> listaDeLivros = new ArrayList<LivroModel>();
 
 	public String SalvarLivro(LivroModel livroModel) {
-
 		listaDeLivros.add(livroModel);
 		livroModel.setQuantidadeDeExemplares(livroModel.getQuantidadeDeExemplares()+1);
 		return "Sucesso";
-
 	}
 
 	public String EditarLivro(String nome, String autor, int anoDeCriacao) {
-
 		if (listaDeLivros.size() > 0) {
-
 			for (LivroModel livroModel : listaDeLivros) {
-
 				livroModel.setNome(nome);
 				livroModel.setAutor(autor);
 				livroModel.setAnoDeCriacao(anoDeCriacao);
-
 			}
-
 			return "Sucesso";
-
 		}
-
 		return "Falha";
-
 	}
 
 	public String inativarOuAtivarLivro(boolean ativoOuInativo) {
-
 		if (listaDeLivros.size() > 0) {
-
 			for (LivroModel livroModel : listaDeLivros) {
-
 				livroModel.setAtivoOuInativo(ativoOuInativo);
-
 			}
-
 			return "Sucesso";
-
 		}
-
 		return "Falha";
-
 	}
-
 }
